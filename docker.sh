41546e5a4c4033bf437f3a7259896fad7c248cb4
meng.chen <meng.chen@17zuoye.com>
2022-03-29 14:50:21 +0800

    feat: updata docker sh

--- docker.sh
+++ docker.sh
@@ -22,8 +22,9 @@ docker run \
 
 # nginx展示文件目录
 # vi /etc/nginx/conf.d/default.conf
-# location / {   
-#   root /usr/share/nginx/html //指定实际目录绝对路径；   
+# location / { 
+#   charset utf-8;             //处理中文显示乱码
+#   root /usr/share/nginx/html;//指定实际目录绝对路径；   
 #   autoindex on;              //开启目录浏览功能；   
 #   autoindex_exact_size off;  //关闭详细文件大小统计，让文件大小显示MB，GB单位，默认为b；   
 #   autoindex_localtime on;    //开启以服务器本地时区显示文件修改日期！   
